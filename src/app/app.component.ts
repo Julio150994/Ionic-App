c675491f9079c290cec81a8d51f9c6e7b100feb1
Julio Muñoz <49689911+Julio1594@users.noreply.github.com>
2021-08-21 17:24:22 +0200

    Aplicación de Ionic del curso KeepCoding

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+
+  constructor() {
+    // realizamos la excepción en el constructor de la AppComponent mediante debugger
+  }
+}
